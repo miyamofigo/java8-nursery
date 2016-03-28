78da99946a7d315a25487ab013c074d83011bcce
Takeo Miyamoto <miyamofigo@gmail.com>
2016-03-28 16:22:12 +0900

    tab alignments has been adjusted.

--- src/main/java/com/miyamofigo/java8/nursery/Result.java
+++ src/main/java/com/miyamofigo/java8/nursery/Result.java
@@ -7,90 +7,90 @@ import java.util.Optional;
 
 final class Result<T,E> {
 
-	// I gotta determine whether these fields should be implemeted like a union or not..
-	private final Optional<T> value;
-	private final Optional<E> reason;
+  // I gotta determine whether these fields should be implemeted like a union or not..
+  private final Optional<T> value;
+  private final Optional<E> reason;
 
-	private final boolean _isOk;
+  private final boolean _isOk;
 
-	private Result(T value, E reason, boolean isOk) { 
-		this.value = isOk ? Optional.of(value) : Optional.empty();
-		this.reason = isOk ? Optional.empty() : Optional.of(reason);
-	 	this._isOk = isOk;	
-	} 
+  private Result(T value, E reason, boolean isOk) { 
+    this.value = isOk ? Optional.of(value) : Optional.empty();
+    this.reason = isOk ? Optional.empty() : Optional.of(reason);
+    this._isOk = isOk;  
+  } 
 
-	public static <T,E> Result<T,E> ok(T value) { 
-		Objects.requireNonNull(value);
-		return new Result<T,E>(value, null, true); 
-	}	
+  public static <T,E> Result<T,E> ok(T value) { 
+    Objects.requireNonNull(value);
+    return new Result<T,E>(value, null, true); 
+  } 
 
-	public static <T,E> Result<T,E> err(E reason) { 
-		Objects.requireNonNull(reason);
-		return new Result<T,E>(null, reason, false); 
-	}	
+  public static <T,E> Result<T,E> err(E reason) { 
+    Objects.requireNonNull(reason);
+    return new Result<T,E>(null, reason, false); 
+  } 
 
-	public boolean isOk() { return _isOk; }
+  public boolean isOk() { return _isOk; }
 
-	public boolean isErr() { return !_isOk; }
+  public boolean isErr() { return !_isOk; }
 
-	public Optional<T> ok() { 
-		if (isOk()) return value; else return Optional.empty();	
-	}
+  public Optional<T> ok() { 
+    if (isOk()) return value; else return Optional.empty(); 
+  }
 
-	public Optional<E> err() { 	
-		if (isErr()) return reason; else return Optional.empty();
-	}
+  public Optional<E> err() {  
+    if (isErr()) return reason; else return Optional.empty();
+  }
 
-	public Result<?,E> map(Function<T,?> mapper) {
-		Objects.requireNonNull(mapper);
-		try { return Result.ok(mapper.apply(ok().get())); } catch (NoSuchElementException e) { return this; }	
-	}
+  public Result<?,E> map(Function<T,?> mapper) {
+    Objects.requireNonNull(mapper);
+    try { return Result.ok(mapper.apply(ok().get())); } catch (NoSuchElementException e) { return this; } 
+  }
 
-	public Result<T,?> mapErr(Function<E,?> mapper) {
-		Objects.requireNonNull(mapper);
-		try { return Result.err(mapper.apply(err().get())); } catch (NoSuchElementException e) { return this; }	
-	}
+  public Result<T,?> mapErr(Function<E,?> mapper) {
+    Objects.requireNonNull(mapper);
+    try { return Result.err(mapper.apply(err().get())); } catch (NoSuchElementException e) { return this; } 
+  }
 
-	public Result<T,E> and(Result<T,E> res) { if (isOk()) return res; else return this; }
+  public Result<T,E> and(Result<T,E> res) { if (isOk()) return res; else return this; }
 
-	public Result<?,E> andThen(Function<T, Result<?,E>> op) {
-		Objects.requireNonNull(op);
-		try { return op.apply((ok().get())); } catch (NoSuchElementException e) { return this; }
-	}	
+  public Result<?,E> andThen(Function<T, Result<?,E>> op) {
+    Objects.requireNonNull(op);
+    try { return op.apply((ok().get())); } catch (NoSuchElementException e) { return this; }
+  } 
 
-	public Result<T,?> or(Result<T,?> res) { if (isErr()) return res; else return this; }
+  public Result<T,?> or(Result<T,?> res) { if (isErr()) return res; else return this; }
 
-	public Result<T,?> orElse(Function<E, Result<T,?>> op) {
-		Objects.requireNonNull(op);
-		try { return op.apply((err().get())); } catch (NoSuchElementException e) { return this; }
-	}
+  public Result<T,?> orElse(Function<E, Result<T,?>> op) {
+    Objects.requireNonNull(op);
+    try { return op.apply((err().get())); } catch (NoSuchElementException e) { return this; }
+  }
 
-	public T unwrapOr(T optb) {
-		try { return ok().get(); } catch (NoSuchElementException e) { return optb; }
-	}
+  public T unwrapOr(T optb) {
+    try { return ok().get(); } catch (NoSuchElementException e) { return optb; }
+  }
 
-	public T unwrapOrElse(Function<E,T> op) {
-		Objects.requireNonNull(op);
-		try { return ok().get(); } catch (NoSuchElementException e) { return op.apply((err().get())); }
-	}
+  public T unwrapOrElse(Function<E,T> op) {
+    Objects.requireNonNull(op);
+    try { return ok().get(); } catch (NoSuchElementException e) { return op.apply((err().get())); }
+  }
 
-	public T unwrap() throws Exception {
-		try { return ok().get(); } 
-		catch (NoSuchElementException e) { 
-			throw new Exception("failed to unwrap on an `error` value: " + reason.get().toString()); 
-		}
-	}
+  public T unwrap() throws Exception {
+    try { return ok().get(); } 
+    catch (NoSuchElementException e) { 
+      throw new Exception("failed to unwrap on an `error` value: " + reason.get().toString()); 
+    }
+  }
 
-	public T expect(String msg) throws Exception {
-		try { return ok().get(); } 
-		catch (NoSuchElementException e) { throw new Exception(msg + reason.get().toString()); }
-	}
+  public T expect(String msg) throws Exception {
+    try { return ok().get(); } 
+    catch (NoSuchElementException e) { throw new Exception(msg + reason.get().toString()); }
+  }
 
-	public E unwrapErr() throws Exception {
-		try { return err().get(); } 
-		catch (NoSuchElementException e) { 
-			throw new Exception("failed to unwrap on an `ok` reason: " + value.get().toString());
-		}
-	}	
+  public E unwrapErr() throws Exception {
+    try { return err().get(); } 
+    catch (NoSuchElementException e) { 
+      throw new Exception("failed to unwrap on an `ok` reason: " + value.get().toString());
+    }
+  } 
 
 }
--- src/test/java/com/miyamofigo/java8/nursery/ResultTest.java
+++ src/test/java/com/miyamofigo/java8/nursery/ResultTest.java
@@ -4,60 +4,60 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ResultTest {
-	@Test 
-	public void testOk() { 
-		Result<Integer, Exception> ok = Result.ok(1); 
-		assert(ok.isOk());
-		assert(ok.ok().get() == 1); 
-		assert(!ok.isErr());
-		assert(!ok.err().isPresent());
-		assertEquals(2, ok.map(i -> i+1).ok().get()); 
-		assertEquals("one", ok.map(i -> "one").ok().get()); 
-		assert(ok.mapErr(str -> "error").ok().get() == 1);
-		assert(ok.and(Result.ok(2)).ok().get() == 2);
-		assertEquals((Integer) 2, ok.andThen(i -> Result.ok(i+1)).ok().get()); 
-		assertEquals(ok, ok.or(Result.ok(2)));
-		assertEquals(ok, ok.orElse(e -> { 
-											 if (e instanceof NullPointerException) return Result.ok(2);
-											 else return Result.ok(3);
-										 }));
-		assert(ok.unwrapOr(2) == 1);
-		assertEquals((Integer) 1, ok.unwrapOrElse(e -> 3));
-	 	try { 
-			assert(ok.unwrap() == 1); 
-			assert(ok.expect("SUCCESS") == 1);
-			ok.unwrapErr();
-			fail();
-		} catch (Exception e) {}	
-	}
+  @Test 
+  public void testOk() { 
+    Result<Integer, Exception> ok = Result.ok(1); 
+    assert(ok.isOk());
+    assert(ok.ok().get() == 1); 
+    assert(!ok.isErr());
+    assert(!ok.err().isPresent());
+    assertEquals(2, ok.map(i -> i+1).ok().get()); 
+    assertEquals("one", ok.map(i -> "one").ok().get()); 
+    assert(ok.mapErr(str -> "error").ok().get() == 1);
+    assert(ok.and(Result.ok(2)).ok().get() == 2);
+    assertEquals((Integer) 2, ok.andThen(i -> Result.ok(i+1)).ok().get()); 
+    assertEquals(ok, ok.or(Result.ok(2)));
+    assertEquals(ok, ok.orElse(e -> { 
+                       if (e instanceof NullPointerException) return Result.ok(2);
+                       else return Result.ok(3);
+                     }));
+    assert(ok.unwrapOr(2) == 1);
+    assertEquals((Integer) 1, ok.unwrapOrElse(e -> 3));
+    try { 
+      assert(ok.unwrap() == 1); 
+      assert(ok.expect("SUCCESS") == 1);
+      ok.unwrapErr();
+      fail();
+    } catch (Exception e) {}  
+  }
 
-	@Test
-	public void testErr() {
-		Result<Integer, Exception> err = Result.err(new Exception("test"));
-		assert(!err.isOk());
-		assert(!err.ok().isPresent());
-		assert(err.isErr());
-		assertEquals("test", err.err().get().getMessage());
-		assert(err.map(i -> i+1).isErr());
-		assertEquals("test", err.mapErr(e -> e.getMessage()).err().get());
-		assert(err.and(Result.ok(1)).isErr());
-		assert(err.andThen(i -> Result.ok(i+1)).isErr());
-		assert(err.or(Result.ok(2)).isOk());
-		assertEquals(Integer.valueOf(3),
-								 err.orElse(e -> {
-									 		 if (e instanceof NullPointerException) return Result.ok(2);
-									 		 else return Result.ok(3);
-								 		 })
-								 		.ok().get());
-		assert(err.unwrapOr(2) == 2);
-		assertEquals((int) 4, (int) err.unwrapOrElse(e -> e.getMessage().length()));
-		try { err.unwrap(); fail(); } 
-		catch (Exception e) { 
-			assertEquals("failed to unwrap on an `error` value: java.lang.Exception: test", 
-									 e.getMessage()); 
-		}
-		try { err.expect("SUCCESS: "); fail(); } 
-		catch (Exception e) { assertEquals("SUCCESS: java.lang.Exception: test", e.getMessage()); }
-		try { assertEquals("test", err.unwrapErr().getMessage()); } catch (Exception e) {}
-	}
+  @Test
+  public void testErr() {
+    Result<Integer, Exception> err = Result.err(new Exception("test"));
+    assert(!err.isOk());
+    assert(!err.ok().isPresent());
+    assert(err.isErr());
+    assertEquals("test", err.err().get().getMessage());
+    assert(err.map(i -> i+1).isErr());
+    assertEquals("test", err.mapErr(e -> e.getMessage()).err().get());
+    assert(err.and(Result.ok(1)).isErr());
+    assert(err.andThen(i -> Result.ok(i+1)).isErr());
+    assert(err.or(Result.ok(2)).isOk());
+    assertEquals(Integer.valueOf(3),
+                 err.orElse(e -> {
+                       if (e instanceof NullPointerException) return Result.ok(2);
+                       else return Result.ok(3);
+                     })
+                    .ok().get());
+    assert(err.unwrapOr(2) == 2);
+    assertEquals((int) 4, (int) err.unwrapOrElse(e -> e.getMessage().length()));
+    try { err.unwrap(); fail(); } 
+    catch (Exception e) { 
+      assertEquals("failed to unwrap on an `error` value: java.lang.Exception: test", 
+                   e.getMessage()); 
+    }
+    try { err.expect("SUCCESS: "); fail(); } 
+    catch (Exception e) { assertEquals("SUCCESS: java.lang.Exception: test", e.getMessage()); }
+    try { assertEquals("test", err.unwrapErr().getMessage()); } catch (Exception e) {}
+  }
 }
